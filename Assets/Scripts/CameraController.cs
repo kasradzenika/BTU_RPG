9c8c778da2d92b33e370b6ab120435109fa0d984
kasradzenika <kasradzenika@yahoo.com>
2017-06-06 21:26:36 +0400

    initial commit: camera movement and player movement using AI

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float speed;
+    public float zoom;
+    public float farZoom;
+    public float nearZoom;
+    
+    void Start ()
+    {
+
+	}
+	
+	void Update ()
+    {
+        MoveCamera();
+        ZoomCamera();
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            FocusOnPlayer();
+        }
+	}
+
+    //TODO: write this function
+    void FocusOnPlayer()
+    {
+        Debug.LogWarning("FUNCTION NOT IMPLEMENTED");
+    }
+
+    void MoveCamera()
+    {
+        Vector3 pos = transform.position;
+
+        //vertical movement
+        if (Input.mousePosition.y >= Screen.height)
+        {
+            pos.z += speed * Time.deltaTime;
+        }
+        else if (Input.mousePosition.y <= 0)
+        {
+            pos.z -= speed * Time.deltaTime;
+        }
+
+        //horizontal movement
+        if (Input.mousePosition.x <= 0)
+        {
+            pos.x -= speed * Time.deltaTime;
+        }
+        else if (Input.mousePosition.x >= Screen.width)
+        {
+            pos.x += speed * Time.deltaTime;
+        }
+        
+        transform.position = pos;
+    }
+
+    void ZoomCamera()
+    {
+        zoom += Input.mouseScrollDelta.y;
+
+        //if (zoom < minZoom)
+        //    zoom = minZoom;
+        //else if (zoom > maxZoom)
+        //    zoom = maxZoom;
+        //is same as this code:
+        zoom = Mathf.Clamp(zoom, farZoom, nearZoom);
+
+        Vector3 pos = Camera.main.transform.localPosition;
+        pos.z = zoom;
+        Camera.main.transform.localPosition = pos;
+    }
+}
---
+++ Assets/Scripts/PlayerMover.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMover : MonoBehaviour
+{
+    public float speed = 3f;
+    public Vector3 targetPosition;
+	
+	void Update ()
+    {
+        MouseClickWatcher();
+        //Move();
+    }
+
+    void MouseClickWatcher()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit raycastHit;
+
+            //cast ray and save information in raycastHit variable
+            Physics.Raycast(ray, out raycastHit);
+
+            if (raycastHit.transform != null)
+            {
+                targetPosition = raycastHit.point;
+                GetComponent<NavMeshAgent>().SetDestination(targetPosition);
+            }
+        }
+    }
+
+    //void Move()
+    //{
+    //    Vector3 direction = targetPosition - transform.position;
+    //    direction = direction.normalized;
+
+    //    transform.position += direction * speed * Time.deltaTime;
+    //}
+}
